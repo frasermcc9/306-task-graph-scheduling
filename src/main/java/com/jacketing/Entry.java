d426aaf2825ca518e458f060e7981aaaffdca556
Fraser McCallum <fraser@themccallums.net.nz>
2021-07-20 13:44:13 +1200

    initial commit

---
+++ src/main/java/com/jacketing/Entry.java
@@ -0,0 +1,7 @@
+package com.jacketing;
+
+public class Entry {
+    public static void main(String[] args) {
+        System.out.println("Hello im working!");
+    }
+}
---
+++ src/test/java/com/jacketing/EntryTest.java
@@ -0,0 +1,17 @@
+package com.jacketing;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EntryTest {
+    @Test
+    public void testTestRunnerRunsTests() {
+        Assert.assertEquals(1 + 1, 2);
+    }
+
+    @Test
+    public void testTestRunnerRunsFailedTests() {
+        Assert.assertEquals(1 + 1, 2);
+    }
+}
