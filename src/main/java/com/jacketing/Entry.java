785bc201a5d5fee63c85948538a4b40dece2f697
Fraser McCallum <fraser@themccallums.net.nz>
2021-07-22 10:17:21 +1200

    Update project level files

--- src/main/java/com/jacketing/Entry.java
+++ src/main/java/com/jacketing/Entry.java
@@ -1,3 +1,14 @@
+// ********************************************************************************
+// Copyright 2021 Team Jacketing All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of Team
+// Jacketing (the author) and their affiliates, if any. The intellectual and
+// technical concepts contained herein are proprietary to Team Jacketing , and
+// are protected by copyright law. Dissemination of this information or
+// reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from the author.
+// *******************************************************************************
+
 package com.jacketing;
 
 public class Entry {
@@ -5,3 +16,5 @@ public class Entry {
         System.out.println("Hello im working!");
     }
 }
+
+
