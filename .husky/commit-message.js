48a2145c3bee57248bfed1715669ebba2ca304d1
Fraser McCallum <87796367+frasermcc9-edu@users.noreply.github.com>
2021-07-27 13:02:14 +1200

    :toolbox: Enforce emojis (#5)
    Co-authored-by: Fraser McCallum <fraser@themccallums.net.nz>

---
+++ .husky/commit-message.js
@@ -0,0 +1,14 @@
+const process = require("process");
+const fs = require("fs");
+
+const args = process.argv;
+const message = args[2];
+
+const commitMsg = fs.readFileSync(message, "utf-8");
+const allowed =
+  /:rocket:|:sparkles:|:bug:|:boom:|:card_index:|:books:|:bulb:|:racehorse:|:lipstick:|:rotating_light:|:zap:|:art:|:hammer:|:fire:|:green_heart:|:toolbox:|:lock:|:arrow_up:|:arrow_down:|:shirt:|:ambulance:|:wrench:|:package:|:truck:|:apple:|:penguin:|:checkered_flag:|:construction:/g;
+
+if (allowed.test(commitMsg)) {
+  process.exit(0);
+}
+process.exit(1);
