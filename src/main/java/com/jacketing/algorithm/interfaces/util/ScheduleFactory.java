35ce2f79bb44ce6305da5ce4a66ab74347781f16
Fraser McCallum <fraser@themccallums.net.nz>
2021-07-31 20:02:18 +1200

    :sparkles: Implement datastructures for the algorithm

---
+++ src/main/java/com/jacketing/algorithm/interfaces/util/ScheduleFactory.java
@@ -0,0 +1,13 @@
+package com.jacketing.algorithm.interfaces.util;
+
+import com.jacketing.algorithm.impl.util.ScheduleFactoryImpl;
+import com.jacketing.algorithm.interfaces.structures.Schedule;
+import com.jacketing.io.cli.ProgramContext;
+
+public interface ScheduleFactory {
+  static ScheduleFactory create() {
+    return new ScheduleFactoryImpl();
+  }
+
+  Schedule newSchedule(ProgramContext ctx);
+}
