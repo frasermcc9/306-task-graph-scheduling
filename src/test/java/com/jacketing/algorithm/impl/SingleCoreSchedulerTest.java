f29b32cb5c0cfaaa82481aad138d68c8a2d7dedb
Fraser McCallum <fraser@themccallums.net.nz>
2021-08-01 01:23:37 +1200

    :shirt: Lint code

--- src/test/java/com/jacketing/algorithm/impl/SingleCoreSchedulerTest.java
+++ src/test/java/com/jacketing/algorithm/impl/SingleCoreSchedulerTest.java
@@ -4,12 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.jacketing.TestUtil;
 import com.jacketing.algorithm.interfaces.SchedulingAlgorithmStrategy;
 import com.jacketing.algorithm.interfaces.structures.Schedule;
 import com.jacketing.algorithm.interfaces.util.ScheduleFactory;
 import com.jacketing.io.cli.ProgramContext;
 import com.jacketing.parsing.impl.structures.Graph;
-import com.jacketing.util.TestUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
