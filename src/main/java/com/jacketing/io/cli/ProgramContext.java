50808f6229c9b3d53568cb36b64f7afc7893be3e
Fraser McCallum <fraser@themccallums.net.nz>
2021-07-31 19:58:38 +1200

    :shirt: :truck: Lint io and move into package

--- src/main/java/com/jacketing/io/cli/ProgramContext.java
+++ src/main/java/com/jacketing/io/cli/ProgramContext.java
@@ -1,9 +1,8 @@
-package com.jacketing;
+package com.jacketing.io.cli;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.validators.PositiveInteger;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +11,20 @@ public class Args {
   @Parameter(description = "Input")
   private List<String> input = new ArrayList<>();
 
-  @Parameter(names = { "-p" }, description = "use N cores for execution in parallel", validateWith=PositiveInteger.class)
+  @Parameter(
+    names = { "-p" },
+    description = "use N cores for execution in parallel",
+    validateWith = PositiveInteger.class
+  )
   private int coresToCalculateWith;
 
   @Parameter(names = { "-v" }, description = "visualize the search")
   private boolean visualize;
 
-  @Parameter(names = { "-o" }, description = "output file is named OUTPUT (default is INPUT-output.dot")
+  @Parameter(
+    names = { "-o" },
+    description = "output file is named OUTPUT (default is INPUT-output.dot"
+  )
   private String outputName;
 
   public String getInputFile() {
@@ -49,30 +55,44 @@ public class Args {
 
   public void validate() throws IllegalArgumentException {
     if (input.size() != 2) {
-      throw new ParameterException("2 arguments expected, received " + input.size());
+      throw new ParameterException(
+        "2 arguments expected, received " + input.size()
+      );
     }
 
     if (!input.get(0).endsWith(".dot")) {
-      throw new ParameterException(".dot file expected as first parameter, received " + input.get(0));
+      throw new ParameterException(
+        ".dot file expected as first parameter, received " + input.get(0)
+      );
     }
 
     try {
       Integer.parseInt(input.get(1));
-    } catch(NumberFormatException e) {
-      throw new ParameterException("Integer expected as second parameter, received " + input.get(1));
+    } catch (NumberFormatException e) {
+      throw new ParameterException(
+        "Integer expected as second parameter, received " + input.get(1)
+      );
     }
   }
 
   public String helpText() {
     StringBuilder output = new StringBuilder();
     output.append("java -jar scheduler.jar INPUT.dot P [OPTIONS]\n");
-    output.append("INPUT.dot: a task graph with integer weights in dot format\n");
-    output.append("P:         number of processors to schedule the INPUT graph on\n");
+    output.append(
+      "INPUT.dot: a task graph with integer weights in dot format\n"
+    );
+    output.append(
+      "P:         number of processors to schedule the INPUT graph on\n"
+    );
     output.append("\n");
     output.append("Optional:\n");
-    output.append("-p N:      use N cores for execution (default sequential)\n");
+    output.append(
+      "-p N:      use N cores for execution (default sequential)\n"
+    );
     output.append("-v:        visualize the search\n");
-    output.append("-o OUTPUT: output file is named OUTPUT (default INPUT-output.dot\n");
+    output.append(
+      "-o OUTPUT: output file is named OUTPUT (default INPUT-output.dot\n"
+    );
 
     return output.toString();
   }
--- src/test/java/com/jacketing/ProgramContextTest.java
+++ src/test/java/com/jacketing/ProgramContextTest.java
@@ -2,9 +2,10 @@ package com.jacketing;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
-import org.junit.Test;
-import org.junit.Before;
+import com.jacketing.io.cli.Args;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ArgsTest {
 
@@ -14,87 +15,94 @@ public class ArgsTest {
   @Before
   public void setupArgs() {
     args = new Args();
-    builder = JCommander.newBuilder()
-      .addObject(args)
-      .build();
+    builder = JCommander.newBuilder().addObject(args).build();
   }
 
   @Test
   public void testInputFile() {
-    String[] input = {"INPUT.dot", "2"};
+    String[] input = { "INPUT.dot", "2" };
     builder.parse(input);
     Assert.assertEquals("INPUT.dot", args.getInputFile());
   }
 
   @Test
   public void testInvalidInputFile() {
-    String[] input = {"INPUT", "2"};
+    String[] input = { "INPUT", "2" };
     builder.parse(input);
 
     try {
       args.validate();
       Assert.fail("Exception expected");
-    } catch(ParameterException e) {
-      Assert.assertEquals(".dot file expected as first parameter, received INPUT", e.getMessage());
+    } catch (ParameterException e) {
+      Assert.assertEquals(
+        ".dot file expected as first parameter, received INPUT",
+        e.getMessage()
+      );
     }
   }
 
   @Test
   public void testCoresScheduleOn() {
-    String[] input = {"INPUT.dot", "2"};
+    String[] input = { "INPUT.dot", "2" };
     builder.parse(input);
     Assert.assertEquals(2, args.getCoresToScheduleOn());
   }
 
   @Test
   public void testInvalidCoresScheduleOn() {
-    String[] input = {"INPUT.dot", "a"};
+    String[] input = { "INPUT.dot", "a" };
     builder.parse(input);
 
     try {
       args.validate();
       Assert.fail("Exception expected");
     } catch (ParameterException e) {
-      Assert.assertEquals("Integer expected as second parameter, received a", e.getMessage());
+      Assert.assertEquals(
+        "Integer expected as second parameter, received a",
+        e.getMessage()
+      );
     }
   }
 
   @Test
   public void testCoresCalculateWith() {
-    String[] input = {"INPUT.dot", "1", "-p", "2"};
+    String[] input = { "INPUT.dot", "1", "-p", "2" };
     builder.parse(input);
     Assert.assertEquals(2, args.getCoresToCalculateWith());
   }
 
   @Test
   public void testInvalidCoresCalculateWith() {
-    String[] input = {"INPUT.dot", "1", "-p", "a"};
+    String[] input = { "INPUT.dot", "1", "-p", "a" };
 
     try {
       builder.parse(input);
       Assert.fail("Exception expected");
     } catch (ParameterException e) {
-      Assert.assertEquals("java.lang.NumberFormatException: For input string: \"a\"", e.getMessage());
+      Assert.assertEquals(
+        "java.lang.NumberFormatException: For input string: \"a\"",
+        e.getMessage()
+      );
     }
   }
 
   @Test
   public void testVisualize() {
-    String[] input = {"INPUT.dot", "2", "-v"};
+    String[] input = { "INPUT.dot", "2", "-v" };
     builder.parse(input);
     Assert.assertEquals(true, args.isVisualized());
   }
 
   @Test
   public void testOutput() {
-    String[] input = {"INPUT.dot", "2", "-o", "Test.dot"};
+    String[] input = { "INPUT.dot", "2", "-o", "Test.dot" };
     builder.parse(input);
     Assert.assertEquals("Test.dot", args.getOutputName());
   }
 
   @Test
   public void testNoOutput() {
-    String[] input = {"INPUT.dot", "2"};
+    String[] input = { "INPUT.dot", "2" };
     builder.parse(input);
     Assert.assertEquals("INPUT-output.dot", args.getOutputName());
   }
