b9e1db3faf955dc432c6f05be2687514f06367a3
nzbasic <jcop589@aucklanduni.ac.nz>
2021-08-02 18:15:31 +1200

    🔥 Remove setContext

--- src/main/java/com/jacketing/algorithm/impl/util/topological/TopologicalSortContext.java
+++ src/main/java/com/jacketing/algorithm/impl/util/topological/TopologicalSortContext.java
@@ -13,10 +13,6 @@ public class TopologicalSortContext<T> implements TopologicalSort<T> {
     this.context = context;
   }
 
-  public void setContext(TopologicalSort<T> context) {
-    this.context = context;
-  }
-
   @Override
   public List<T> sortedTopological() {
     return context.sortedTopological();
