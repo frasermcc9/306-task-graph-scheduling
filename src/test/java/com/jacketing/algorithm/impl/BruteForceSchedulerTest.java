745b90a28576b851b32553f991061b344b26d9c1
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2021-08-04 04:07:04 +1200

    :rotating_light: Add test cases for brute foce algo

---
+++ src/test/java/com/jacketing/algorithm/impl/BruteForceSchedulerTest.java
@@ -0,0 +1,78 @@
+package com.jacketing.algorithm.impl;
+
+import com.jacketing.TestUtil;
+import com.jacketing.algorithm.interfaces.SchedulingAlgorithmStrategy;
+import com.jacketing.algorithm.interfaces.structures.Schedule;
+import com.jacketing.algorithm.interfaces.util.ScheduleFactory;
+import com.jacketing.io.cli.ProgramContext;
+import com.jacketing.parsing.impl.structures.Graph;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+  import static org.mockito.Mockito.when;
+
+  import com.jacketing.TestUtil;
+  import com.jacketing.algorithm.interfaces.SchedulingAlgorithmStrategy;
+  import com.jacketing.algorithm.interfaces.structures.Schedule;
+  import com.jacketing.algorithm.interfaces.util.ScheduleFactory;
+  import com.jacketing.io.cli.ProgramContext;
+  import com.jacketing.parsing.impl.structures.Graph;
+  import org.junit.After;
+  import org.junit.Before;
+  import org.junit.Test;
+
+public class BruteForceSchedulerTest {
+
+  @Test
+  public void testScheduleWithOneProcessor() {
+    Graph graph = TestUtil.graphVariantOne();
+    ProgramContext programContext = mock(ProgramContext.class);
+    when(programContext.getCoresToScheduleOn()).thenReturn(1);
+
+    SchedulingAlgorithmStrategy schedulingAlgorithmStrategy = SchedulingAlgorithmStrategy.create(
+      new BruteForceScheduler(graph, programContext, ScheduleFactory.create())
+    );
+
+    Schedule schedule = schedulingAlgorithmStrategy.schedule();
+    schedule.getDuration();
+
+    assertEquals(10, schedule.getDuration());
+  }
+  @Test
+  public void testScheduleWithTwoProcessors() {
+    Graph graph = TestUtil.graphVariantOne();
+    ProgramContext programContext = mock(ProgramContext.class);
+    when(programContext.getCoresToScheduleOn()).thenReturn(2);
+
+    SchedulingAlgorithmStrategy schedulingAlgorithmStrategy = SchedulingAlgorithmStrategy.create(
+      new BruteForceScheduler(graph, programContext, ScheduleFactory.create())
+    );
+
+    Schedule schedule = schedulingAlgorithmStrategy.schedule();
+    schedule.getDuration();
+
+    assertEquals(8, schedule.getDuration());
+  }
+
+  @Test
+  public void testScheduleWithThreeProcessors() {
+    Graph graph = TestUtil.graphVariantOne();
+    ProgramContext programContext = mock(ProgramContext.class);
+    when(programContext.getCoresToScheduleOn()).thenReturn(3);
+
+    SchedulingAlgorithmStrategy schedulingAlgorithmStrategy = SchedulingAlgorithmStrategy.create(
+      new BruteForceScheduler(graph, programContext, ScheduleFactory.create())
+    );
+
+    Schedule schedule = schedulingAlgorithmStrategy.schedule();
+    schedule.getDuration();
+
+    assertEquals(8, schedule.getDuration());
+  }
+}
