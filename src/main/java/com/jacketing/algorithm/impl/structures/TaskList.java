7965a22d0e8d5bed613b8582f8836f8d3d0e86d0
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2021-08-03 17:46:34 +1200

    :zap: Add getter for last scheduled task on a proc

--- src/main/java/com/jacketing/algorithm/impl/structures/TaskList.java
+++ src/main/java/com/jacketing/algorithm/impl/structures/TaskList.java
@@ -1,11 +1,23 @@
 package com.jacketing.algorithm.impl.structures;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class TaskList extends ArrayList<Task> {
 
+  public TaskList() {
+    super();
+  }
+
+  public TaskList(List<Task> tasks) {
+    super(tasks);
+  }
   public int getLastScheduledEndTime() {
     if (this.isEmpty()) return 0;
     return this.get(this.size() - 1).getEndTime();
+  }
+
+  public Task getLastScheduledTask(){
+    return this.get(this.size()-1);
   }
 }
