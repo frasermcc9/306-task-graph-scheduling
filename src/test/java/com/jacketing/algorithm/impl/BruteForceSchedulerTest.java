fbe46b54fc76b5c121c2b33da883f75b82535911
Fraser McCallum <fraser@themccallums.net.nz>
2021-08-04 15:56:21 +1200

    :rotating_light: Add graph checking suite

--- src/test/java/com/jacketing/algorithm/impl/BruteForceSchedulerTest.java
+++ src/test/java/com/jacketing/algorithm/impl/BruteForceSchedulerTest.java
@@ -5,11 +5,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.jacketing.TestUtil;
+import com.jacketing.TestUtil.GraphResult;
 import com.jacketing.algorithm.interfaces.SchedulingAlgorithmStrategy;
 import com.jacketing.algorithm.interfaces.structures.Schedule;
 import com.jacketing.algorithm.interfaces.util.ScheduleFactory;
 import com.jacketing.io.cli.ProgramContext;
 import com.jacketing.parsing.impl.structures.Graph;
+import java.io.IOException;
+import java.util.List;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BruteForceSchedulerTest {
@@ -60,5 +64,53 @@ public class BruteForceSchedulerTest {
     schedule.getDuration();
 
     assertEquals(8, schedule.getDuration());
+  }
+
+  @Test
+  @Ignore
+  public void testGraphSuiteTwoCores() throws IOException {
+    List<GraphResult> graphs = TestUtil.getGraphTestSuite();
+
+    ProgramContext programContext = mock(ProgramContext.class);
+    when(programContext.getProcessorsToScheduleOn()).thenReturn(2);
+
+    for (GraphResult graph : graphs) {
+      SchedulingAlgorithmStrategy schedulingAlgorithmStrategy = SchedulingAlgorithmStrategy.create(
+        new BruteForceScheduler(
+          graph.getGraph(),
+          programContext,
+          ScheduleFactory.create()
+        )
+      );
+
+      Schedule schedule = schedulingAlgorithmStrategy.schedule();
+      int optimalLength = schedule.getDuration();
+
+      assertEquals(graph.getTwoCoresResult(), optimalLength);
+    }
+  }
+
+  @Test
+  @Ignore
+  public void testGraphSuiteFourCores() throws IOException {
+    List<GraphResult> graphs = TestUtil.getGraphTestSuite();
+
+    ProgramContext programContext = mock(ProgramContext.class);
+    when(programContext.getProcessorsToScheduleOn()).thenReturn(4);
+
+    for (GraphResult graph : graphs) {
+      SchedulingAlgorithmStrategy schedulingAlgorithmStrategy = SchedulingAlgorithmStrategy.create(
+        new BruteForceScheduler(
+          graph.getGraph(),
+          programContext,
+          ScheduleFactory.create()
+        )
+      );
+
+      Schedule schedule = schedulingAlgorithmStrategy.schedule();
+      int optimalLength = schedule.getDuration();
+
+      assertEquals(graph.getFourCoresResult(), optimalLength);
+    }
   }
 }
