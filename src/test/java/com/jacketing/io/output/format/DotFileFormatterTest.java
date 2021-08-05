573992f3fec55ccaec2e34cfe19140161a8737a5
Bruce Zeng <bruce.zeng@xtra.co.nz>
2021-08-05 17:21:38 +1200

    :construction: Set up DotFileFormatterTest

---
+++ src/test/java/com/jacketing/io/output/format/DotFileFormatterTest.java
@@ -0,0 +1,11 @@
+package com.jacketing.io.output.format;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class DotFileFormatterTest {
+
+  @Before
+  public void setUp() throws Exception {}
+}
