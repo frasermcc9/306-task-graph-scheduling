8d59474ae75b02ed82161e8708d928eb6192a48e
BJ <kelmeld@outlook.com>
2021-08-09 18:17:35 +1200

    :sparkles: add a stacked bar chart

--- src/main/java/com/jacketing/view/VisualizationController.java
+++ src/main/java/com/jacketing/view/VisualizationController.java
@@ -1,5 +1,6 @@
 package com.jacketing.view;
 
+import com.jacketing.algorithm.impl.structures.Task;
 import com.jacketing.util.CPU.CpuReader;
 import com.jacketing.util.CPU.CpuStatModel;
 import com.jacketing.util.RAM.RamReader;
@@ -7,9 +8,13 @@ import com.jacketing.util.RAM.RamStatModel;
 import javafx.fxml.FXML;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 
 public class VisualizationController {
+
+  @FXML
+  private StackedBarChart<String, Double> bestScheduleGraph;
 
   @FXML
   private LineChart<String, Double> thread1Graph;
@@ -28,6 +33,65 @@ public class VisualizationController {
 
   @FXML
   public void initialize() {
+    XYChart.Series<String, Double> procSeries = new XYChart.Series();
+
+    // data with delays
+    Task[] taskList = {
+      new Task(3, 3, 0),
+      new Task(6, 3, 0),
+      new Task(10, 2, 0),
+    };
+
+    //delayed start
+    if (taskList[0].getStartTime() != 0) {
+      XYChart.Data<String, Double> bar = new XYChart.Data(
+        "2",
+        taskList[0].getStartTime()
+      );
+      bar
+        .nodeProperty()
+        .addListener((ov, oldNode, newNode) -> newNode.setVisible(false));
+      procSeries.getData().add(bar);
+    }
+    // then add the first task
+    XYChart.Data<String, Double> firstBar = new XYChart.Data(
+      "2",
+      taskList[0].getDuration()
+    );
+    firstBar
+      .nodeProperty()
+      .addListener(
+        (ov, oldNode, newNode) -> newNode.setStyle("-fx-border-color: black")
+      );
+    procSeries.getData().add(firstBar);
+
+    // the rest of the tasks
+    for (int i = 1; i < taskList.length; i++) {
+      // check for delays (if this task starts later than the previous end time)
+      if (taskList[i - 1].getEndTime() != taskList[i].getStartTime()) {
+        XYChart.Data<String, Double> bar = new XYChart.Data(
+          "2",
+          taskList[i].getStartTime() - taskList[i - 1].getEndTime()
+        );
+        bar
+          .nodeProperty()
+          .addListener((ov, oldNode, newNode) -> newNode.setVisible(false));
+        procSeries.getData().add(bar);
+      }
+      // now just add the current task
+      XYChart.Data<String, Double> bar = new XYChart.Data(
+        "2",
+        taskList[i].getDuration()
+      );
+      bar
+        .nodeProperty()
+        .addListener(
+          (ov, oldNode, newNode) -> newNode.setStyle("-fx-border-color: black")
+        );
+      procSeries.getData().add(bar);
+    }
+    bestScheduleGraph.getData().addAll(procSeries);
+
     LineChart[] allUpdatingCharts = {
       thread1Graph,
       thread2Graph,
