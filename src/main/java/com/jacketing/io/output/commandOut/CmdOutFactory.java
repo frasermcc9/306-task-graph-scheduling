a87c7610838198f2ad9d7332aff992fa4cc5bdb6
Bruce Zeng <bruce.zeng@xtra.co.nz>
2021-08-10 22:18:04 +1200

    :sparkles: Create a factory for CommandLineOutput

--- src/main/java/com/jacketing/io/output/commandOut/CmdOutFactory.java
+++
@@ -1,8 +0,0 @@
-package com.jacketing.io.output.commandOut;
-
-import com.jacketing.io.cli.ApplicationContext;
-import com.jacketing.io.cli.IOContext;
-
-public interface CmdOutFactory {
-  void printCmdOutput(ApplicationContext context);
-}
