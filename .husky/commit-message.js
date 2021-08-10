32e0057e47e141216c1df0337c6db00a356c22eb
Bruce Zeng <bruce.zeng@xtra.co.nz>
2021-08-10 22:55:57 +1200

    :wrench: Add new emoji to contributing emoji lists

--- .husky/commit-message.js
+++ .husky/commit-message.js
@@ -6,7 +6,7 @@ const message = args[2];
 
 const commitMsg = fs.readFileSync(message, "utf-8");
 const allowed =
-  /Merge|:rocket:|:sparkles:|:bug:|:boom:|:card_index:|:books:|:bulb:|:racehorse:|:lipstick:|:rotating_light:|:zap:|:art:|:hammer:|:fire:|:green_heart:|:toolbox:|:lock:|:arrow_up:|:arrow_down:|:shirt:|:ambulance:|:wrench:|:package:|:truck:|:apple:|:penguin:|:checkered_flag:|:construction:/g;
+  /Merge|:rocket:|:sparkles:|:bug:|:boom:|:card_index:|:books:|:bulb:|:racehorse:|:lipstick:|:rotating_light:|:zap:|:art:|:hammer:|:fire:|:green_heart:|:toolbox:|:lock:|:arrow_up:|:arrow_down:|:shirt:|:ambulance:|:wrench:|:package:|:truck:|:apple:|:penguin:|:checkered_flag:|:construction:|:rage:/g;
 
 if (allowed.test(commitMsg)) {
   process.exit(0);
