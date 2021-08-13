9246ea86e3b0f768f2254a4f3e7ddabacdfa913d
Bruce Zeng <bruce.zeng@xtra.co.nz>
2021-08-13 22:23:37 +1200

    :toolbox: add csv output for average times

--- tools/benchmarking/benchmark.js
+++ tools/benchmarking/benchmark.js
@@ -4,7 +4,6 @@
 const fs = require("fs");
 const path = require("path");
 const exec = require("child_process").exec;
-const readline = require("readline");
 
 const graphDirectory = path.join(__dirname, "graphs");
 
@@ -32,46 +31,80 @@ const removeOutputFiles = async () => {
   console.log(`Tried removing ${outputFilenames.length} files`);
 };
 
-const parseBenchmarkOutput = (stdoutstr) => {
-  console.log(stdoutstr);
-};
-
-const runBenchmark = async (filename) => {
+const runBenchmark = async (filename, processorCount) => {
   const fullPath = path.join(__dirname, `graphs/${filename}`);
   const jarPath = path.join(__dirname, "306-a1-1.0-SNAPSHOT.jar");
-  const command = `java -jar "${jarPath}" "${fullPath}" 2`;
+  const command = `java -jar "${jarPath}" "${fullPath}" ${processorCount}`;
   console.log(`running benchmark: [${filename}]`);
   return new Promise((resolve, reject) => {
-    exec(command, (err, stdout) => {
+    exec(command, (err) => {
       if (err) {
         console.error(err);
         reject(err);
       } else {
-        console.log(stdout);
         resolve();
       }
     });
   });
 };
 
-const benchmark = async () => {
+const appendResultsToCsv = (testName, processorCount, averageTime) => {
+  // time
+  const currentDate = new Date().toISOString();
+
+  const fpCsv = path.join(__dirname, "test.csv");
+  const data = `${currentDate},${testName},${processorCount},${averageTime}\n`;
+  fs.appendFileSync(fpCsv, data);
+};
+
+const benchmark = async (processorCount) => {
   const filesNames = getGraphFileNames();
 
   console.log(`Found ${filesNames.length} files to benchmark`);
   console.log("Running benchmark now...");
 
-  for (let i = 3; i < filesNames.length; i++) {
-    const filename = filesNames[i];
-    const validResult = await runBenchmark(filename);
-    console.log("Result");
-    if (validResult) parseBenchmarkOutput(validResult);
+  const sortedFn = filesNames.sort();
+
+  const testGroup = {}; // testGroup[testName] = testFilesArray[]
+
+  sortedFn.forEach((f) => {
+    const x = f.split("_");
+    const testName = x.slice(0, -1).join("_");
+
+    let testFilesArray = testGroup[testName];
+    if (testFilesArray === undefined) testFilesArray = [];
+
+    testFilesArray.push(f);
+    testGroup[testName] = testFilesArray;
+  });
+
+  let totalTime = 0;
+
+  for (const testName of Object.keys(testGroup)) {
+    const testFilesArray = testGroup[testName];
+    console.log("----------------------------------------");
+    console.log(`Now testing ${testName} found ${testFilesArray.length} tests`);
+
+    for (const filename of testFilesArray) {
+      const t0 = performance.now();
+      await runBenchmark(filename, processorCount);
+      const t1 = performance.now();
+      console.log("result");
+      const currentTime = t1 - t0;
+      totalTime += currentTime;
+    }
+    const averageTime = parseFloat((totalTime / 3).toFixed(3));
+
+    // append to csv
+    appendResultsToCsv(testName, processorCount, averageTime);
   }
 };
 
 const main = async () => {
   await removeOutputFiles();
   // fileNames should be all valid now
-  await benchmark();
+  await benchmark(4);
+  //await benchmark(2); ** UNCOMMENT WHEN INDEPENDENTNODES ARE FIXED IN ENTRY
 };
 
 main().then();
